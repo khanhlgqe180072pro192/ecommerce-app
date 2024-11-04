8526ffaf7bdcc8e8c2f4fb8c83ffdc7cc9e94020
khanh gia <luonggiakhanh1997@gmail.com>
2024-11-04 21:56:43 +0700

    new update

--- frontend/src/components/OurPolicy.jsx
+++ frontend/src/components/OurPolicy.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { assets } from '../assets/assets'
 
-const Ourpolicy = () => {
+const OurPolicy = () => {
   return (
     <div className='flex flex-col sm:flex-row justify-around gap-12 sm:gap-2 text-center py-20 text-xs sm:text-sm md:text-base text-gray-700'>
       <div>
@@ -23,4 +23,4 @@ const Ourpolicy = () => {
   )
 }
 
-export default Ourpolicy
+export default OurPolicy
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import Hero from '../components/Hero'
 import LastestCollection from '../components/LastestCollection'
 import BestSeller from '../components/BestSeller'
-import Ourpolicy from '../components/OurPolicy'
+import OurPolicy from '../components/OurPolicy'
 import NewsletterBox from '../components/NewsletterBox'
 
 const Home = () => {
@@ -11,7 +11,7 @@ const Home = () => {
       <Hero />
       <LastestCollection />
       <BestSeller/>
-      <Ourpolicy/>
+      <OurPolicy/>
       <NewsletterBox/>
     </div>
   )
