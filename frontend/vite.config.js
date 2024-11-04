f52a91bd292b2423261387204714a3f4fcb3f0ee
khanh gia <luonggiakhanh1997@gmail.com>
2024-11-04 22:19:26 +0700

    new update

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -7,7 +7,8 @@ export default defineConfig({
   server: {port:5173},
   build: {
     rollupOptions: {
-      external: ['react-router-dom']
+      external: ['react-router-dom'],
+      external: ['react-toastify']
     }
   }
 })
