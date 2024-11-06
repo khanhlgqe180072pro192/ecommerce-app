cfdf37750ba044c5beff60b92b86a3b768e30762
khanh gia <luonggiakhanh1997@gmail.com>
2024-11-06 16:14:49 +0700

    new update

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,14 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {port:5173},
-  build: {
-    rollupOptions: {
-      external: [
-        'react-toastify', 
-        'react-router-dom',
-        'react-toastify/dist/ReactToastify.css' // Add the CSS file as external
-      ]
-    }
-  }
+  server: {port:5173}
 })
